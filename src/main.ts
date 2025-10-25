02a7f5ed56ffb65c52dbc99c4c0b38935495ca2e
mateus.walz <mateus.walz@gmail.com>
2025-10-25 11:45:53 -0300

    remove serveless libs

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  if (process.env.NODE_ENV !== 'production') {
-    const config = new DocumentBuilder()
-      .setTitle('API')
-      .setDescription('API Docs')
-      .setVersion('1.0')
-      .build();
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('docs', app, document);
-  }
+  // Habilita CORS se necessário
+  app.enableCors();
 
   await app.listen(3000);
 }

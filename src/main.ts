eb6bf2bc219217fe6d62d881f49c2ac9a2dede2a
mateus.walz <mateus.walz@gmail.com>
2025-10-25 12:11:58 -0300

    add bycript

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // Habilita CORS se necessário
-  app.enableCors();
-
   await app.listen(3000);
 }
 bootstrap();

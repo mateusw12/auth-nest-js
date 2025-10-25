f9f16b003d4561c3d7eda4cad87592420cdd0900
mateus.walz <mateus.walz@gmail.com>
2025-10-25 19:40:41 -0300

    add api property on dto

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsString()
   username: string;
 
+  @ApiProperty()
   @IsString()
   @MinLength(4)
   password: string;

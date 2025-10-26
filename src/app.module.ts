4de9649e77a8d1b4878677863a77bd04cf176f8d
mateus.walz <mateus.walz@gmail.com>
2025-10-26 10:38:28 -0300

    add throttler

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,30 @@ import { AuthModule } from './auth/auth.module';
 import { ItemModule } from './item/item.module';
 import { ApiKeyModule } from './api-key/api-key.module';
 import { BasicAuthModule } from './basic-auth/basic-auth.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [ItemModule, UserModule, BasicAuthModule, ApiKeyModule, AuthModule],
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60, // tempo de vida da janela (60 segundos)
+        limit: 10, // número máximo de requisições permitidas
+      },
+    ]),
+    ItemModule,
+    UserModule,
+    BasicAuthModule,
+    ApiKeyModule,
+    AuthModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard, // aplica globalmente o guard
+    },
+  ],
 })
 export class AppModule {}

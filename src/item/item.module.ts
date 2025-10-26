ded3927d5dd45150f34105ee22bea8266b37ec0e
mateus.walz <mateus.walz@gmail.com>
2025-10-26 11:39:49 -0300

    remove cache interceptor

--- src/item/item.module.ts
+++ src/item/item.module.ts
@@ -2,19 +2,11 @@ import { Module } from '@nestjs/common';
 import { ItemController } from './item.controller';
 import { ItemService } from './item.service';
 import { AuthModule } from 'src/auth/auth.module';
-import { APP_INTERCEPTOR } from '@nestjs/core';
-import { CacheInterceptor } from 'src/common/interceptors/cache.interceptor';
 
 @Module({
   imports: [AuthModule],
   controllers: [ItemController],
-  providers: [
-    ItemService,
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: CacheInterceptor, // aplica a todas as rotas do módulo
-    },
-  ],
+  providers: [ItemService],
   exports: [ItemService],
 })
 export class ItemModule {}
